fc63b49ebd592cfe07402792bbad9ead4b698ccd
aikmeng <aikmeng80@gmail.com>
2016-05-20 23:20:56 +0800

    Add new intelli project files and unit tests.

--- src/test/java/FieldTest.java
+++ src/test/java/FieldTest.java
@@ -21,7 +21,12 @@ public class FieldTest {
     @Test
     public void testParseFromString() {
         Field classUnderTest = new Field(MAX_COLUMN, MAX_ROW);
-        String fieldString = "1,1,1,1,1,1,1;1,1,1,1,1,1,1;1,1,1,1,1,1,1;1,1,1,1,1,1,1;1,1,1,1,1,1,1;1,1,1,1,1,1,1";
+        String fieldString =    "1,1,1,1,1,1,1;" +
+                                "1,1,1,1,1,1,1;" +
+                                "1,1,1,1,1,1,1;" +
+                                "1,1,1,1,1,1,1;" +
+                                "1,1,1,1,1,1,1;" +
+                                "1,1,1,1,1,1,1";
         classUnderTest.parseFromString(fieldString);
 
         for(int currentRow = MAX_ROW - 1; currentRow >= 0; currentRow--) {
@@ -29,5 +34,60 @@ public class FieldTest {
                 assertEquals(1, classUnderTest.getDisc(currentColumn, currentRow));
             }
         }
+    }
+
+    @Test
+    public void testIsColumnFull() {
+        Field classUnderTest = new Field(MAX_COLUMN, MAX_ROW);
+        String fieldString =    "0,0,1,0,0,0,0;" +
+                                "0,0,1,0,0,0,0;" +
+                                "0,0,1,0,0,0,0;" +
+                                "0,0,1,0,0,0,0;" +
+                                "0,0,1,0,0,0,0;" +
+                                "0,0,1,0,0,0,0";
+
+        classUnderTest.parseFromString(fieldString);
+        assertTrue(classUnderTest.isColumnFull(2));
+    }
+
+    @Test
+    public void testIsValidMove_returns_true() {
+        Field classUnderTest = new Field(MAX_COLUMN, MAX_ROW);
+        String fieldString =    "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,1,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0";
+
+        classUnderTest.parseFromString(fieldString);
+        assertTrue(classUnderTest.isValidMove(2));
+    }
+
+    @Test
+    public void testIsValidMove_returns_false() {
+        Field classUnderTest = new Field(MAX_COLUMN, MAX_ROW);
+        String fieldString =    "0,0,2,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0";
+
+        classUnderTest.parseFromString(fieldString);
+        assertFalse(classUnderTest.isValidMove(2));
+    }
+
+    @Test
+    public void testIsFull() {
+        Field classUnderTest = new Field(MAX_COLUMN, MAX_ROW);
+        String fieldString =    "1,1,1,1,1,1,1;" +
+                                "1,1,1,1,1,1,1;" +
+                                "1,1,1,1,1,1,1;" +
+                                "1,1,1,1,1,1,1;" +
+                                "1,1,1,1,1,1,1;" +
+                                "1,1,1,1,1,1,1";
+        classUnderTest.parseFromString(fieldString);
+        assertTrue(classUnderTest.isFull());
     }
 }
