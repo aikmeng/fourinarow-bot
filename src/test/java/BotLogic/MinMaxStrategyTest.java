bc7092dcebf8e7df8dcca5159bf0acc4243b4b8c
aikmeng <aikmeng80@gmail.com>
2016-06-11 23:19:33 +0800

    Add test case to min max strategy.

--- src/test/java/BotLogic/MinMaxStrategyTest.java
+++ src/test/java/BotLogic/MinMaxStrategyTest.java
@@ -25,4 +25,52 @@ public class MinMaxStrategyTest {
         MinMaxStrategy classUnderTest = new MinMaxStrategy(botId, playboard);
         assertEquals(3, classUnderTest.makeTurn());
     }
+
+    @Test
+    public void testMakeTurn_Bot_Id_Two() {
+        Playboard playboard = new Playboard(MAX_COLUMN, MAX_ROW);
+        String fieldString =    "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "1,0,0,0,0,0,0";
+        playboard.parseFromString(fieldString);
+        int botId = 2;
+
+        MinMaxStrategy classUnderTest = new MinMaxStrategy(botId, playboard);
+        assertEquals(3, classUnderTest.makeTurn());
+    }
+
+    @Test
+    public void testMakeTurn_Find_Winner_Column_case_1() {
+        Playboard playboard = new Playboard(MAX_COLUMN, MAX_ROW);
+        String fieldString =    "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "1,0,0,0,0,0,0;" +
+                                "1,0,0,0,0,0,0;" +
+                                "1,2,2,0,0,2,0";
+        playboard.parseFromString(fieldString);
+        int botId = 1;
+
+        MinMaxStrategy classUnderTest = new MinMaxStrategy(botId, playboard);
+        assertEquals(0, classUnderTest.makeTurn());
+    }
+
+    @Test
+    public void testMakeTurn_Block_Winner_Column_case_1() {
+        Playboard playboard = new Playboard(MAX_COLUMN, MAX_ROW);
+        String fieldString =    "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "0,0,0,0,0,0,0;" +
+                                "1,0,0,0,0,0,0;" +
+                                "1,2,2,0,2,1,0";
+        playboard.parseFromString(fieldString);
+        int botId = 1;
+
+        MinMaxStrategy classUnderTest = new MinMaxStrategy(botId, playboard);
+        assertEquals(0, classUnderTest.makeTurn());
+    }
 }
